a1ee4bdd28c15c7edbbeedbaaac4c7d928be923a
Toni R <trahyns@gmail.com>
2020-10-11 23:38:16 -0400

    update todo name in title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
-            <h3 className="text-capitalize text-center">todo input</h3>
+            <h3 className="text-capitalize text-center">to-do input</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
