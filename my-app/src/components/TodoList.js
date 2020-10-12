3b5d46acc0b47719f5c58f27c1b26e67b7e502ff
Toni R <trahyns@gmail.com>
2020-10-11 23:38:29 -0400

    update todo name in title

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -5,7 +5,7 @@ export default class TodoList extends Component {
         const { items, clearList, handleDelete, handleEdit } = this.props;
         return (
             <ul className="list-group my-5">
-                <h3 className="text-capitalize text-center">todo list</h3>
+                <h3 className="text-capitalize text-center">to-do list</h3>
                 {items.map(item => {
                     return (
                         <TodoItem
